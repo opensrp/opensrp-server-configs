2e92f30e186766e4693c5b0e6226220b70257bba
Samuel Githengi <samuel.githengi@gmail.com>
2019-05-03 19:50:07 +0300

    Add Script for installing postgres

---
+++ assets/migrations/install_postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >  /etc/apt/sources.list.d/pgdg.list
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+sudo apt-get -yq update
+
+sudo apt-get -yq install postgresql-10
+
+sudo apt-get -yq  install postgresql-10-postgis-2.4 postgresql-10-postgis-2.4-scripts
