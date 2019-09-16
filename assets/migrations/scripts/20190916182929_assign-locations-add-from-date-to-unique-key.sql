84961093f5e0c2cecd3de00c0b8707ee53552bf5
Samuel Githengi <samuel.githengi@gmail.com>
2019-09-16 21:34:26 +0300

    Add date from to location assignment index

---
+++ assets/migrations/scripts/20190916182929_assign-locations-add-from-date-to-unique-key.sql
@@ -0,0 +1,28 @@
+--
+--    Copyright 2010-2016 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+-- // aassign-locatins-add-from-date-to-unique-key
+-- Migration SQL that makes the change goes here.
+
+
+DROP INDEX team.organization_location_organization_id_location_id_plan_id_key RESTRICT;
+CREATE UNIQUE INDEX organization_location_organization_id_location_id_plan_id_key ON team.organization_location (organization_id, location_id, plan_id, from_date);
+-- //@UNDO
+-- SQL to undo the change goes here.
+DROP INDEX team.organization_location_organization_id_location_id_plan_id_key RESTRICT;
+CREATE UNIQUE INDEX organization_location_organization_id_location_id_plan_id_key ON team.organization_location (organization_id, location_id, plan_id);
+
+
