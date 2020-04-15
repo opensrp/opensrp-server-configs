69ba554c02667de430def2596ab85e32dc0adbac
Vincent Karuri <vincentmkaruri@gmail.com>
2020-04-15 11:06:22 +0300

    Move settings migration script to migrations folder

--- assets/migrations/scripts/20200415080451_migrate_settings_data_into_settings_metadata_columns_uuid_json_setting_type_setting_value_setting_key_setting_description_inherited_from.sql
+++ assets/migrations/scripts/20200415080451_migrate_settings_data_into_settings_metadata_columns_uuid_json_setting_type_setting_value_setting_key_setting_description_inherited_from.sql
@@ -1,3 +1,21 @@
+--
+--    Copyright 2010-2016 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+-- // migrate_settings_data_into_settings_metadata_columns_uuid_json_setting_type_setting_value_setting_key_setting_description_inherited_from
+-- Migration SQL that makes the change goes here.
 SET search_path to core;
 
 CREATE OR REPLACE FUNCTION parse_json ()
@@ -35,3 +53,9 @@ $$
 $$ LANGUAGE 'plpgsql';
 
 SELECT parse_json();
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
