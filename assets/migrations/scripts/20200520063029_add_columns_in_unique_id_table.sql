fb07074130ead08060cda7edf9f48ebbb7e46260
rehammuzzamil <62061579+rehammuzzamil@users.noreply.github.com>
2020-05-29 11:48:11 +0500

    310 : Modification in unique_id table

---
+++ assets/migrations/scripts/20200520063029_add_columns_in_unique_id_table.sql
@@ -0,0 +1,24 @@
+-- // add columns in unique_id table
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE core.unique_id ADD COLUMN identifier VARCHAR;
+
+ALTER TABLE core.unique_id ADD COLUMN id_source bigserial;
+
+ALTER TABLE core.unique_id ADD COLUMN is_reserved BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE core.unique_id ADD CONSTRAINT fk_identifier_source_id FOREIGN KEY (id_source) REFERENCES core.identifier_source(id);
+
+CREATE INDEX unique_id_identifier_index ON core.unique_id (identifier);
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+ALTER TABLE core.unique_id DROP CONSTRAINT fk_identifier_source_id;
+
+DROP INDEX unique_id_identifier_index;
+
+ALTER TABLE core.unique_id DROP COLUMN identifier;
+
+ALTER TABLE core.unique_id DROP COLUMN id_source;
+
+ALTER TABLE core.unique_id DROP COLUMN is_reserved;
