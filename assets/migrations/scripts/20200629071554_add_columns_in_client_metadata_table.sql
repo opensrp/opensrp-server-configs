59d94ec97775a3bdbf3ba612b065be1f542ee40b
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-29 14:57:55 +0500

    258 : Add client_type and location_id column in client metadata table

---
+++ assets/migrations/scripts/20200629071554_add_columns_in_client_metadata_table.sql
@@ -0,0 +1,17 @@
+-- // add columns in client metadata table
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE core.client_metadata ADD COLUMN location_id VARCHAR DEFAULT NULL;
+ALTER TABLE core.client_metadata ADD COLUMN client_type VARCHAR DEFAULT NULL;
+
+CREATE INDEX client_metadata_location_id_index ON core.client_metadata(location_id);
+CREATE INDEX client_metadata_client_type_index ON core.client_metadata(client_type);
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP INDEX core.client_metadata_location_id_index;
+DROP INDEX core.client_metadata_client_type_index;
+
+ALTER TABLE core.client_metadata DROP COLUMN location_id;
+ALTER TABLE core.client_metadata DROP COLUMN client_type;
+
