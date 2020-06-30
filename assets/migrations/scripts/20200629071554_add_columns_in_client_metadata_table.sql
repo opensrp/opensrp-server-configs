a809ea1c80c08773478345e1d9d3399f02938abe
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-30 12:23:26 +0500

    258

--- assets/migrations/scripts/20200629071554_add_columns_in_client_metadata_table.sql
+++ assets/migrations/scripts/20200629071554_add_columns_in_client_metadata_table.sql
@@ -4,13 +4,11 @@
 ALTER TABLE core.client_metadata ADD COLUMN location_id VARCHAR DEFAULT NULL;
 ALTER TABLE core.client_metadata ADD COLUMN client_type VARCHAR DEFAULT NULL;
 
-CREATE INDEX client_metadata_location_id_index ON core.client_metadata(location_id);
-CREATE INDEX client_metadata_client_type_index ON core.client_metadata(client_type);
+CREATE INDEX client_metadata_location_id_client_type_index ON core.client_metadata(location_id,client_type);
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-DROP INDEX core.client_metadata_location_id_index;
-DROP INDEX core.client_metadata_client_type_index;
+DROP INDEX core.client_metadata_location_id_client_type_index;
 
 ALTER TABLE core.client_metadata DROP COLUMN location_id;
 ALTER TABLE core.client_metadata DROP COLUMN client_type;
