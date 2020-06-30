ba1959d7c907dcf412d7f1c942a3b425a06b1be3
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-30 12:52:13 +0500

    259

--- assets/migrations/scripts/20200629122125_add_columns_in_event_metadata_table.sql
+++ assets/migrations/scripts/20200629122125_add_columns_in_event_metadata_table.sql
@@ -3,8 +3,12 @@
 
 ALTER TABLE core.event_metadata ADD COLUMN plan_identifier VARCHAR DEFAULT NULL;
 
+CREATE INDEX event_metadata_plan_identifier_index ON core.event_metadata(plan_identifier);
+
 -- //@UNDO
 -- SQL to undo the change goes here.
+
+DROP INDEX core.event_metadata_plan_identifier_index;
 
 ALTER TABLE core.event_metadata DROP COLUMN plan_identifier;
 
