01aadf8ea720c0a6028f8b7bdee317688db3e47e
rehammuzzamil <62061579+rehammuzzamil@users.noreply.github.com>
2020-07-03 13:55:29 +0500

    Update assets/migrations/scripts/20200629122125_add_columns_in_event_metadata_table.sql
    Co-authored-by: Samuel Githengi <samuel.githengi@gmail.com>

--- assets/migrations/scripts/20200629122125_add_columns_in_event_metadata_table.sql
+++ assets/migrations/scripts/20200629122125_add_columns_in_event_metadata_table.sql
@@ -6,7 +6,7 @@ ALTER TABLE core.event_metadata ADD COLUMN plan_identifier VARCHAR DEFAULT NULL;
 CREATE INDEX event_metadata_plan_identifier_index ON core.event_metadata(plan_identifier);
 
 UPDATE core.event_metadata
-SET plan_identifier = (select json ->> 'plan_identifier' from core.event where event.id = event_metadata.event_id);
+SET plan_identifier = (select json->'details'->>'planIdentifier' from core.event where event.id = event_metadata.event_id);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
@@ -14,4 +14,3 @@ SET plan_identifier = (select json ->> 'plan_identifier' from core.event where e
 DROP INDEX core.event_metadata_plan_identifier_index;
 
 ALTER TABLE core.event_metadata DROP COLUMN plan_identifier;
-
