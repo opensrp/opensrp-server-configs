abd9a1aa015eaff833655af6c6639c066dd910e4
Reham Muzzamil <reham.muzzamil@venturedive.com>
2020-09-01 20:33:49 +0500

    89 : Create Product Catalog table

--- assets/migrations/scripts/20200827132736_create_product_catalogue_table.sql
+++ assets/migrations/scripts/20200827132736_create_product_catalogue_table.sql
@@ -1,15 +1,14 @@
 -- // create supply catalog table
 -- Migration SQL that makes the change goes here.
 
-CREATE TABLE core.supply_catalog
+CREATE TABLE core.product_catalogue
 (
-    id character varying NOT NULL,
+    unique_id bigserial NOT NULL,
     product_name character varying NOT NULL,
     type character varying NOT NULL,
-    unique_id bigserial NOT NULL ,
     json jsonb NOT NULL,
     server_version bigint,
-    PRIMARY KEY (id)
+    PRIMARY KEY (unique_id)
 )
 WITH (
     OIDS = FALSE
@@ -19,4 +18,4 @@ WITH (
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-DROP TABLE core.supply_catalog;
+DROP TABLE core.product_catalogue;
