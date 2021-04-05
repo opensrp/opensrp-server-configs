6c584daffe9527d73a6e6e69ea6a088c109f8057
Allan O <a.onchuru@gmail.com>
2021-04-05 14:11:02 +0300

    :card_file_box: Add create status table SQL script

---
+++ assets/migrations/scripts/20210401165933_create_sms_api_processing_status_table.sql
@@ -0,0 +1,28 @@
+-- // create sms api processing status table
+-- Migration SQL that makes the change goes here.
+
+CREATE TABLE core.sms_api_processing_status
+(
+  id bigserial NOT NULL,
+  base_entity_id character varying UNIQUE NOT NULL,
+  event_type character varying NOT NULL,
+  service_type character varying NOT NULL,
+  request_status character varying NOT NULL,
+  date_created timestamp,
+  last_updated timestamp,
+  sms_delivery_status character varying,
+  attempts int NOT NULL,
+  sms_delivery_date timestamp,
+  PRIMARY KEY (id)
+)
+WITH (
+  OIDS = FALSE
+) TABLESPACE ${core_tablespace};
+
+CREATE INDEX sms_api_processing_status_base_entity_id_index ON core.sms_api_processing_status (base_entity_id);
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
+DROP TABLE core.sms_api_processing_status;
