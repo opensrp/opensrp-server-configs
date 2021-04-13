a756a6034a647beb5bb33f7822edf8507fb20242
Allan O <a.onchuru@gmail.com>
2021-04-13 18:07:18 +0300

    :construction: Add request_id column

--- assets/migrations/scripts/20210401165933_create_sms_api_processing_status_table.sql
+++ assets/migrations/scripts/20210401165933_create_sms_api_processing_status_table.sql
@@ -4,10 +4,11 @@
 CREATE TABLE core.sms_api_processing_status
 (
   id bigserial NOT NULL,
-  base_entity_id character varying UNIQUE NOT NULL,
+  base_entity_id character varying NOT NULL,
   event_type character varying NOT NULL,
   service_type character varying NOT NULL,
   request_status character varying NOT NULL,
+  request_id character varying UNIQUE NOT NULL,
   date_created timestamp,
   last_updated timestamp,
   sms_delivery_status character varying,
@@ -19,7 +20,7 @@ WITH (
   OIDS = FALSE
 ) TABLESPACE ${core_tablespace};
 
-CREATE INDEX sms_api_processing_status_base_entity_id_index ON core.sms_api_processing_status (base_entity_id);
+CREATE INDEX sms_api_processing_status_request_id_index ON core.sms_api_processing_status (request_id);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
