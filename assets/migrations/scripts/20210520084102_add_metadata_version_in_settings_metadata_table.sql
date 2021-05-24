b98a6722f6700613aaca8c00ca5b86eb8735c324
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-05-24 14:07:22 +0500

    830 : Add metadata_version sequence and column in settings_metadata table

---
+++ assets/migrations/scripts/20210520084102_add_metadata_version_in_settings_metadata_table.sql
@@ -0,0 +1,11 @@
+-- // add metadata_version in settings_metadata table
+-- Migration SQL that makes the change goes here.
+
+CREATE SEQUENCE IF NOT EXISTS core.setting_metadata_version_seq START 1;
+ALTER TABLE core.settings_metadata ADD COLUMN metadata_version bigint DEFAULT nextval('core.setting_metadata_version_seq');
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE core.settings_metadata DROP COLUMN metadata_version;
+DROP SEQUENCE IF EXISTS core.setting_metadata_version_seq;
